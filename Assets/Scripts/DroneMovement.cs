b4cde16ef4402be9c30ab8fa1fa9bbda97ed7b2b
Masha <n3mm@mail.ru>
2026-02-04 17:10:38 +0300

    Движение дрона и камеры

--- Assets/Scripts/DroneMovement.cs
+++ Assets/Scripts/DroneMovement.cs
@@ -1,8 +1,9 @@
+using System;
 using UnityEngine;
 
 public class DroneMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 0.1f;
     private Vector3 target;
     private bool isMoving = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,8 +15,24 @@ public class DroneMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) { 
-            
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.CompareTag("WaterPath")) { 
+                    target = hit.point;
+                    isMoving = true;
+                    print("target: "+target);
+                }
+            }
+        }
+
+        if (isMoving) { 
+            transform.LookAt(target);
+            transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.deltaTime);
+            if (Vector3.Distance(target, transform.position) < 0.01f) { 
+                isMoving = false;
+            }
         }
     }
 }
