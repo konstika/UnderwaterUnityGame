fe1b5805ed84f3c80bcce281177f70d0f890baef
Masha <n3mm@mail.ru>
2026-02-04 17:48:21 +0300

    Движение по зажатой

--- Assets/Scripts/DroneMovement.cs
+++ Assets/Scripts/DroneMovement.cs
@@ -15,7 +15,7 @@ public class DroneMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButton(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
