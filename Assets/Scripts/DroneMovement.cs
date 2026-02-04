6c95f0604e7cf41fe6b37577f96ded4010841767
Masha <n3mm@mail.ru>
2026-02-04 15:57:36 +0300

    Настройка прототипа

---
+++ Assets/Scripts/DroneMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    private Vector3 target;
+    private bool isMoving = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            
+        }
+    }
+}
